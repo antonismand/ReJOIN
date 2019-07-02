a311da30119d118ea8ce89bfdce6385c5a397329
DelphianCalamity <kelkost@yahoo.gr>
2019-07-02 18:38:33 +0300

    skeleton

--- src/state.py
+++ src/state.py
@@ -1,18 +1,27 @@
 from moz_sql_parser import parse
 import json
 
-class State:
+class StateVector:
 
     def __init__(self, sql_query):
 
         self.query = sql_query
-        self.initial_state = self.extract_initial_state(sql_query)
 
-    def extract_initial_state(self, sql_query):
+        self.query_ast = json.dumps(parse(sql_query))
+        print(self.query_ast)
+        self.tree_structure = self.extract_tree_structure()
+        self.join_predicates = self.extract_join_predicates()
+        self.selection_predicates = self.extract_selection_predicates()
 
-        ast = json.dumps(parse(sql_query))
 
-        print(ast)
+    def extract_tree_structure(self, sql_query):
 
         return None
 
+    def extract_join_predicates(self, sql_query):
+
+        return None
+
+    def extract_selection_predicates(self, sql_query):
+
+        return None
